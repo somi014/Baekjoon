019cbca95967e2bb5fcfd9b595e972d08a2a12d2
somi014 <somi112005@gmail.com>
2023-08-12 00:53:08 +0900

    백준 1662번 문제 풀이

---
+++ 1662.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool visit[51];
+
+int dfs(string& S, int idx) 
+{
+    int cnt = 0;
+
+    for (int i = idx; i < S.size(); i++) 
+    {
+        if (S[i] == '(' && !visit[i]) 
+        {
+            visit[i] = true;
+            int num = S[i - 1] - '0';           //괄호 앞 = 반복 횟수
+            cnt--;                              //반복 횟수는 개수에서 제외
+            cnt += num * dfs(S, i + 1);         //반복 횟수 * 압축 푼 문자열
+        }
+        else if (S[i] == ')' && !visit[i]) 
+        {
+            visit[i] = true;
+            return cnt;
+        }
+        else if (!visit[i]) 
+        {
+            visit[i] = true;
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+int main() 
+{
+    string s;
+
+    cin >> s;
+
+    cout << dfs(s, 0) << endl;
+    return 0;
+}
