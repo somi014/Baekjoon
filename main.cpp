0691db27f8c55760915cfe4db92d01c4f928efc4
Dasom <dasom@salin.co.kr>
2025-11-10 16:10:45 +0900

    5597번

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    vector<bool> student(31, false);
+    int number;
+
+    for (int i = 1; i <= 28; ++i)
+    {
+        cin >> number;
+        student[number] = true;
+    }
+
+    for (int i = 1; i <= 30; ++i)
+    {
+        int number;
+
+        if (student[i] == 0)
+        {
+            cout << i << endl;
+        }
+    }
     return 0;
 }
