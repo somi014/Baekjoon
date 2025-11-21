dbd3ec5cf1750ff84295b8761f08515c83f25961
Dasom <dasom@salin.co.kr>
2025-11-21 16:24:12 +0900

    7662번 map

--- main.cpp
+++ main.cpp
@@ -1,44 +1,61 @@
 #include <iostream>
 #include <string>
-#include <queue>
+#include <map>
 #include <iomanip>
 #include <cmath>
 using namespace std;
-
-struct Compare
-{
-    bool operator()(int a, int b) const
-    {
-        if (abs(a) == abs(b))
-            return a > b;       // 절댓값 같으면 원래 값이 작은 게 먼저
-        return abs(a) > abs(b); // 절댓값 기준 오름차순
-    }
-};
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+    int T;
+    cin >> T;
 
-    priority_queue<int, vector<int>, Compare> pq;
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < T; i++)
     {
-        int num;
-        cin >> num;
+        int K;
+        cin >> K;
 
-        if (num == 0)
+        map<int, int> Q;
+        for (int j = 0; j < K; j++)
         {
-            cout << (pq.empty() ? 0 : pq.top()) << "\n";
-            if (!pq.empty())
-                pq.pop();
+            char cmd;
+            int x;
+
+            cin >> cmd >> x;
+            if (cmd == 'I')
+            {
+                Q[x]++;
+            }
+            else if (cmd == 'D')
+            {
+                if (Q.empty())
+                    continue;
+
+                if (x == 1)
+                {
+                    auto it = --Q.end();
+                    if (it->second == 1)
+                        Q.erase(it);
+                    else
+                        it->second--;
+                }
+                else
+                {
+                    auto it = Q.begin();
+                    if (it->second == 1)
+                        Q.erase(it);
+                    else
+                        it->second--;
+                }
+            }
         }
+        if (Q.empty())
+            cout << "EMPTY\n";
         else
-        {
-            pq.push(num);
-        }
+            cout << (--Q.end())->first << ' ' << Q.begin()->first << '\n';
     }
 
     return 0;
