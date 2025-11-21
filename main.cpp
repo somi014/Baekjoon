9c3aead19afd26de67e9738e1d942cbbad53925d
Dasom <dasom@salin.co.kr>
2025-11-21 23:15:44 +0900

    21939번 최대 최소   map<int, set<int>>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <set>
 #include <iomanip>
 #include <cmath>
 using namespace std;
@@ -10,52 +11,65 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
-    cin >> T;
+    int N;
+    cin >> N;
 
-    for (int i = 0; i < T; i++)
+    map<int, set<int>> problems; // 난이도별 문제 번호 기록(같은 난이도의 여러 문제)
+    map<int, int> problem_level; // 문제 번호별 난이도 기록  (문제 삭제 시 키 찾기용)
+    for (int i = 0; i < N; i++)
     {
-        int K;
-        cin >> K;
+        int P;
+        int L;
+        cin >> P >> L;
 
-        map<int, int> Q;
-        for (int j = 0; j < K; j++)
+        problems[L].insert(P);
+        problem_level[P] = L;
+    }
+
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++)
+    {
+        string c;
+        cin >> c;
+
+        if (c == "add")
         {
-            char cmd;
-            int x;
-
-            cin >> cmd >> x;
-            if (cmd == 'I')
+            int P;
+            int L;
+            cin >> P >> L;
+            problems[L].insert(P);
+            problem_level[P] = L;
+        }
+        else if (c == "solved")
+        {
+            int P;
+            cin >> P;
+            int L = problem_level[P];
+            problems[L].erase(P);
+            problem_level.erase(P);
+            if (problems[L].empty())
             {
-                Q[x]++;
-            }
-            else if (cmd == 'D')
-            {
-                if (Q.empty())
-                    continue;
-
-                if (x == 1)
-                {
-                    auto it = --Q.end();
-                    if (it->second == 1)
-                        Q.erase(it);
-                    else
-                        it->second--;
-                }
-                else
-                {
-                    auto it = Q.begin();
-                    if (it->second == 1)
-                        Q.erase(it);
-                    else
-                        it->second--;
-                }
+                problems.erase(L);
             }
         }
-        if (Q.empty())
-            cout << "EMPTY\n";
-        else
-            cout << (--Q.end())->first << ' ' << Q.begin()->first << '\n';
+        else if (c == "recommend")
+        {
+            int P;
+            cin >> P;
+            if (P == 1)
+            {
+                auto it = prev(problems.end());
+                int level = *it->second.rbegin();
+                cout << level << '\n';
+            }
+            else
+            {
+                auto it = problems.begin();
+                int level = *it->second.begin();
+                cout << level << '\n';
+            }
+        }
     }
 
     return 0;
